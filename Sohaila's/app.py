7cf41054ca0b9e10df6a14965fd77bac443d3923
S-haila <sohailanazari07@gmail.com>
2023-12-19 20:04:33 +1100

    pie chart codes up

--- Sohaila's/app.py
+++ Sohaila's/app.py
@@ -1,30 +1,19 @@
-from sqlalchemy import create_engine, func
+from flask import Flask, render_template
+from sqlalchemy import create_engine
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from flask import Flask, render_template, jsonify
 from collections import OrderedDict
-
-
 
 #################################################
 # Database Setup
 #################################################
 
-# try:
-#     engine = create_engine("postgresql://sohailanazari07:bg1m9VKeRNvx@ep-sweet-meadow-71567163.us-east-2.aws.neon.tech/disasters?sslmode=require")
-#                        connect_args={"host": "ep-sweet-meadow-71567163.us-east-2.aws.neon.tech", "port": 5432, "sslmode": "require", "options": "-c statement_timeout=10000"})
-# except Exception as e:
-#     print(f"Error connecting to the database: {e}")
-
 engine = create_engine("postgresql://sohailanazari07:bg1m9VKeRNvx@ep-sweet-meadow-71567163.us-east-2.aws.neon.tech/disasters?options=endpoint%3Dep-sweet-meadow-71567163")
 
-# reflect an existing database into a new model
 Base = automap_base()
-# reflect the tables
 Base.prepare(autoload_with=engine)
 
 print(Base.classes.keys())
-# Save reference to the table
 disasterdata = Base.classes.disasters
 
 #################################################
@@ -32,38 +21,21 @@ disasterdata = Base.classes.disasters
 #################################################
 app = Flask(__name__)
 
-
 #################################################
-# Flask Routes 
+# Flask Routes
 #################################################
 
-# @app.route("/")
-# def welcome():
-#     """List all available api routes."""
-#     return (
-#         f"Available Routes:<br/>"
-#         f"/api/v1.0/names<br/>"
-#     )
+@app.route("/")
+def welcome():
+    return render_template("index.html")
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route("/api/v1.0/names")
+@app.route("/get_data")
 def dataset():
-    # Create our session (link) from Python to the DB
     session = Session(engine)
-
-    """Return a list of all column data available"""
-    # Query all passengers
     results = session.query(disasterdata.id, disasterdata.category, disasterdata.sub_category, disasterdata.description, disasterdata.startdate,
                              disasterdata.enddate, disasterdata.latitude,disasterdata.logitude, disasterdata.injuries, disasterdata.deaths, disasterdata.regions).all()
-    
-    
-    print(results)
     session.close()
-
-    
+   
     finalresults = []
     for i,j,k,l,m,n,o,p,q,r,s in results:
         dataresults = OrderedDict()
@@ -80,9 +52,24 @@ def dataset():
         dataresults["regions"] = s
         finalresults.append(dataresults)
         
-        
     return {"results": finalresults} 
 
+@app.route("/get_transformed_data")
+def transformeddataset():
+    session = Session(engine)
+    results = session.query( disasterdata.category, disasterdata.injuries, disasterdata.deaths, disasterdata.regions).all()
+    session.close()
+   
+    finalresults = []
+    for a,b,c,d in results:
+        dataresults = OrderedDict()
+        dataresults["category"] = a
+        dataresults["injuries"] = b
+        dataresults["deaths"] = c
+        dataresults["regions"] = d
+        finalresults.append(dataresults)
+        
+    return {"results": finalresults} 
 
 if __name__ == '__main__':
     app.run(debug=True)
